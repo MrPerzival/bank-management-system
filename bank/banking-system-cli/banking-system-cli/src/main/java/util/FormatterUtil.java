32addbb6c8cae496aaf3821944b8789e3fc72dcd
Sudhanshu Ranjan Gupta <118988331+MrPerzival@users.noreply.github.com>
2025-07-17 02:54:55 +0530

    Delete bank/banking-system-cli/banking-system-cli directory

--- bank/banking-system-cli/banking-system-cli/src/main/java/util/FormatterUtil.java
+++
@@ -1,8 +0,0 @@
-package util;
-
-import java.time.format.DateTimeFormatter;
-
-public class FormatterUtil {
-    public static final DateTimeFormatter TIMESTAMP_FORMATTER = 
-        DateTimeFormatter.ofPattern("HH:mm:ss dd MMMM yyyy");
-}
